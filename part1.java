5369b4ae9bcf454fb65945b2396ae81f9267ff36
turtle7373 <118865861+turtle7373@users.noreply.github.com>
2022-11-22 21:39:38 -0600

    Add files via upload

---
+++ part1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+public class part1{
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter id:");
+		String id = scan.next();
+		if(id.length() != 5) {
+			System.out.println("enter id that is 5 digits long:");
+			id = scan.next();
+		}
+		else {
+		}		
+		System.out.println("enter input:");
+		int state = 1;
+		String input = scan.next();
+		String ucode = id + "1";
+		String lcode = id + "4";
+		//System.out.println(ucode);
+		//System.out.println(lcode);
+		//System.out.println(input);
+		
+		int i = 0;
+		while(i<input.length()) {
+			if(input.charAt(i) == id.charAt(0)) {
+				int j = i;
+				int pt = 0;
+				while(j<input.length() && pt<ucode.length() && (ucode.charAt(pt)==input.charAt(j)
+						|| lcode.charAt(pt)==input.charAt(j) )) {
+					j++;
+					pt++;
+				}
+		
+				if(pt == id.length()+1) {
+					String code = input.substring(i,j);
+					//System.out.println("CODE DETECTED : "+ code );
+					if(code.equals(ucode)) {
+						state=0;
+					}else {
+						state=1;
+					}
+					
+				}
+				
+			}
+			i++;
+		}
+		// one is locked 0 is unlocked
+		if (state == 1) {
+			System.out.println("Final State is locked");
+		}
+		if (state == 0) {
+			System.out.println("Final State is unlocked");
+		}		
+
+	}
+}
